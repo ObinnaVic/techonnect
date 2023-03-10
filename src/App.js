0cda9889b1c5122d2e307642bfbba25220ab951b
Uche O. Emmanuel <ucheodilachi@gmail.com>
2023-03-10 20:56:11 +0100

    made changes on the team and service page. finished contact page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import './App.css';
-import Data from "./Datas/TeamData"
-import Team from "./components/Team"
 import TeamProfile from "./components/TeamProfile"
-import Service from './components/Service';
 import ServiceContent from './components/ServiceContent';
-import Data2 from './Datas/ServiceData';
 import Contact from './components/Contact';
 function App() {
-   const Teambody = Data.map((item) => {
-    return < TeamProfile
-      key={item.id}
-      {...item}
-      />
-   })
-  const ServiceBody = Data2.map((props) => {
-    return < ServiceContent
-      key={props.id}
-      {...props}
-      />
-  })
+   
   return (
     <div>
       
-      <Team />
-      <section className='teamProfile'> {Teambody}</section>
-      <Service />
-      <section className='servicemain'> {ServiceBody}</section>
+      <TeamProfile />
+      <ServiceContent/>
       <Contact />
 
     </div>
--- src/components/ServiceContent.js
+++ src/components/ServiceContent.js
@@ -1,20 +1,37 @@
 import React from 'react'
+import Data2 from '../Datas/ServiceData';
 
-const ServiceContent = (props) => {
+const ServiceContent = () => {
   return (
-      <>
-          
-        <div className='servicebox'>
-              <div className='text-2xl mb-5'>
-                  {props.title}
-              </div>  
-              <div className='text-xs mt-12'>
-                  {props.content}
-              </div>  
+    <>
+        <header>
+    
+        </header>
+        <div className='service'>
+          <div className='main serviceimg'>
+
           </div>
-           
-        
-    </>
+            <div className='writeup'>
+              <h2>Dive into the details!</h2>
+            </div>
+        </div>
+        <section className='servicemain'>
+          {Data2.map((props) => {
+            return (
+              <div className='servicebox'>
+                  <div className='text-2xl mb-5'>
+                      {props.title}
+                  </div>  
+                  <div className='text-xs mt-12'>
+                      {props.content}
+                  </div>  
+              </div>
+            )
+          })}
+        </section>
+            
+          
+      </>
   )
 };
 
